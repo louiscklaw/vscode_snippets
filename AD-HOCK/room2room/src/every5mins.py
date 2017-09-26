c24d2f5e2a19b0d456ff663ad19da25ba6ee78d9
devin <devin.chen@tinklabs.com>
2017-09-26 14:03:32 +0800

    add dial call to schedule

--- AD-HOCK/room2room/src/every5mins.py
+++ AD-HOCK/room2room/src/every5mins.py
@@ -19,11 +19,21 @@ def jobB():
     # print("aaa")
     osCommand('python handyCall_R2R_Receiver.py')
 
+def dial_Sender():
+    # print("test")
+    osCommand('python handyCall_Dial_Sender.py')
+
+def dial_Receiver():
+    # print("aaa")
+    osCommand('python handyCall_Dial_Receiver.py')
+
 def jobRecord():
     print(datetime.datetime.time(datetime.datetime.now()))
 
 scheduler = BlockingScheduler()
-scheduler.add_job(jobA, 'cron', minute='*/5')
-scheduler.add_job(jobB, 'cron', minute='*/5')
+scheduler.add_job(jobA, 'cron', minute='5,20,35,50')
+scheduler.add_job(jobB, 'cron', minute='5,20,35,50')
+scheduler.add_job(dial_Sender, 'cron', minute='10,25,40,55')
+scheduler.add_job(dial_Receiver, 'cron', minute='10,25,40,55')
 scheduler.add_job(jobRecord, 'interval', seconds=30)
 scheduler.start()
