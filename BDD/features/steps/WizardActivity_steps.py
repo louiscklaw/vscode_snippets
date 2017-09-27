1f3ab7254144c6508e44fbec98d7d731513ef11e
louis.law <louis.law@tinklabs.com>
2017-09-27 11:35:52 +0800

    try simplifying steps

--- BDD/features/steps/WizardActivity_steps.py
+++ BDD/features/steps/WizardActivity_steps.py
@@ -64,8 +64,9 @@ def step_impl(context, Target):
     dWizardActivityPage[WV_SKIP_CHECKOUT_DATE]=u'''
         # bypass checkout day currently, i should press the skip button
         Then Wait until "When are you checking out?" appears on screen, timeout "10" seconds
-          And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
-          And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
+        #   And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
+        #   And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
+          And tap on text "SKIP"
     '''
 
     dWizardActivityPage[WV_SKIP_HANDYMEMBER] =u'''
@@ -73,24 +74,21 @@ def step_impl(context, Target):
         Then sleep 1 seconds
           And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
           And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
+          And tap on text "I'll do it later"
     '''
 
     dWizardActivityPage[WV_SKIP_PERSONALIZED_EXPERIENCE]=u'''
         # Then bypass personalize experience registration currently, i should press the skip button
         Then sleep 1 seconds
-          And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
-          And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
-
-        # # Optional dialogue appears on linux
-        # Then Wait until "Use USB to transfer files?" appears on screen, timeout "60" seconds
-        #   And tap on text "CANCEL"
-
+        #   And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
+        #   And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
+          And tap on text "SKIP"
     '''
 
     dWizardActivityPage[WV_PASS_PLAY_VIDEO]=u'''
-        # wait a moment for video to load
-        Then Wait until Video"com.tinklabs.launcher:id/video_view" appears, timeout 60 seconds
-          And Fail if the Video"com.tinklabs.launcher:id/video_view" not appears
+        # # wait a moment for video to load
+        # Then Wait until Video"com.tinklabs.launcher:id/video_view" appears, timeout 60 seconds
+        #   And Fail if the Video"com.tinklabs.launcher:id/video_view" not appears
 
         # Let's start should appears
         Then Wait until "Let's Start" appears on screen, timeout "180" seconds
