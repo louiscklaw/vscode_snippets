717a4bb353c3e38d6ad5ebdd9e7f7c12f2754192
louis.law <louis.law@tinklabs.com>
2017-09-28 16:53:41 +0800

    clear tinklabs related settings , to make a lighter erase environment rather that FASTBOOT erase

--- BDD/features/steps/handy_steps.py
+++ BDD/features/steps/handy_steps.py
@@ -12,3 +12,27 @@ from android_function import finger
 def PATH(p): return os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
+
+
+@step(u'Clear all handy settings')
+def step_impl(context):
+    """
+        clear tinklabs related settings
+        to make a lighter erase environment rather that FASTBOOT erase
+    """
+    context.execute_steps(u'''
+        Given sleep 1 seconds
+          And adb shell "pm clear com.tinklabs.activateapp"
+          And adb shell "pm clear com.tinklabs.handyappinstaller"
+          And adb shell "pm clear com.tinklabs.handybeacon"
+          And adb shell "pm clear com.tinklabs.handychat"
+          And adb shell "pm clear com.tinklabs.handyclock"
+          And adb shell "pm clear com.tinklabs.handydeals"
+          And adb shell "pm clear com.tinklabs.handyphone"
+          And adb shell "pm clear com.tinklabs.handyremotedebug"
+          And adb shell "pm clear com.tinklabs.handyxposed"
+          And adb shell "pm clear com.tinklabs.launcher"
+          And adb shell "pm clear com.tinklabs.otaupdate"
+          And adb shell "pm clear com.tinklabs.softsim"
+          And adb shell "pm clear com.tinklabs.wifiscanner"
+    ''')
