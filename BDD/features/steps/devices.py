aa04d7179a946174d17774017e6c5f00dcf3b3f9
louis.law <louis.law@tinklabs.com>
2017-10-12 16:24:42 +0800

    add DUMMY_TAP for T1, to tackle "@step(u'Wait until "{Text}" {appears} on screen, timeout "{TimeOut}" seconds')"

--- BDD/features/steps/devices.py
+++ BDD/features/steps/devices.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
+
 class BasicDeviceConfig():
     # default configuration, based on T1
-    DUMMY_TAP = (0,8192)
+    DUMMY_TAP = (0, 1201)
 
     # for the very beginning checkbox
-    GREETING_TAC_CHECKBOX = (60,1242)
+    GREETING_TAC_CHECKBOX = (60, 1242)
     pass
+
 
 class Device_T1(BasicDeviceConfig):
-
+    DUMMY_TAP = (0, 1201)
     pass
+
 
 class Device_M812(BasicDeviceConfig):
-    GREETING_TAC_CHECKBOX=(71,1856)
+    GREETING_TAC_CHECKBOX = (71, 1856)
 
-    DUMMY_TAP=(0,1921)
+    DUMMY_TAP = (0, 1921)
     pass
-
-
