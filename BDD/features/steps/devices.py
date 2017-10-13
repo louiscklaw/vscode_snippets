a98a94250b0d316ecd995bd753b5308ecb2d014e
louis.law <louis.law@tinklabs.com>
2017-10-13 18:43:46 +0800

    update for device config, add GREETING_TAC_CHECKBOX and BUMMY_TAP,

--- BDD/features/steps/devices.py
+++ BDD/features/steps/devices.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# coding:utf-8
+import os
+import sys
+import logging
+import traceback
+from pprint import pprint
 
 
 class BasicDeviceConfig():
@@ -11,12 +17,18 @@ class BasicDeviceConfig():
 
 
 class Device_T1(BasicDeviceConfig):
+    # NOTE: places for tapping to keep/refresh the screens
+    GREETING_TAC_CHECKBOX = (60, 1242)
+
     DUMMY_TAP = (0, 1201)
+
     pass
 
 
 class Device_M812(BasicDeviceConfig):
+    # NOTE: for the checkbox at the bottom right corner of welcome screen
     GREETING_TAC_CHECKBOX = (71, 1856)
 
     DUMMY_TAP = (0, 1921)
+
     pass
