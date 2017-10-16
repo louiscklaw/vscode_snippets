e890468cc5f0cf0194c3379918ebf568fa9a4651
louis.law <louis.law@tinklabs.com>
2017-10-16 10:18:18 +0800

    tidy format,

--- BDD/features/steps/WizardActivity_config.py
+++ BDD/features/steps/WizardActivity_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
-import os, sys
+import os
+import sys
 import logging
 
 from behave import given, when, then, step
@@ -11,25 +12,25 @@ sys.path.append(os.path.dirname(__file__) + '/../_lib')
 from android_function import finger
 
 
-INIT=0
-WV_GREETING=1
-WV_SKIP_CHECKOUT_DATE=2
-WV_SKIP_HANDYMEMBER=3
-WV_SKIP_PERSONALIZED_EXPERIENCE=4
-WV_PASS_PLAY_VIDEO=5
-WV_PASS_TUTORIAL_IMAGE=6
+INIT = 0
+WV_GREETING = 1
+WV_SKIP_CHECKOUT_DATE = 2
+WV_SKIP_HANDYMEMBER = 3
+WV_SKIP_PERSONALIZED_EXPERIENCE = 4
+WV_PASS_PLAY_VIDEO = 5
+WV_PASS_TUTORIAL_IMAGE = 6
 
 
 # default configuration for T1
-dWizardActivityPage={}
-dWizardActivityPage[INIT]=u'''
+dWizardActivityPage = {}
+dWizardActivityPage[INIT] = u'''
     Then tap on position "0","0" using adb
         And sleep 3 seconds
         And Fail if the Text "English" not appears on screen
 '''
 
 # default for T1
-dWizardActivityPage[WV_GREETING]=u'''
+dWizardActivityPage[WV_GREETING] = u'''
     # check the box next to Terms and Conds
         Then tap on position "60","1242" using adb
         And sleep 5 seconds
@@ -38,28 +39,28 @@ dWizardActivityPage[WV_GREETING]=u'''
 '''
 
 # Checkout Calendar, Checkout Calendar Skippable
-dWizardActivityPage[WV_SKIP_CHECKOUT_DATE]=u'''
+dWizardActivityPage[WV_SKIP_CHECKOUT_DATE] = u'''
     # bypass checkout day currently, i should press the skip button
-    # Then Wait until "When are you checking out?" appears on screen, timeout "10" seconds
+    # Then Wait until "When are you checking out?" appears on screen, timeout "60" seconds
     #   And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
     #   And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
     #  And tap on text "SKIP"
 '''
 
 # handy Login, handy Login Skippable
-dWizardActivityPage[WV_SKIP_HANDYMEMBER] =u'''
+dWizardActivityPage[WV_SKIP_HANDYMEMBER] = u'''
     # Then bypass facebook registration currently, i should press the skip button
-    Then Wait until Text startwith "Become a handy member!" appears on screen, timeout "10" seconds
+    Then Wait until Text startwith "Become a handy member!" appears on screen, timeout "60" seconds
     Then sleep 1 seconds
     #   And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
     #   And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
         And tap on text "I'll do it later"
 '''
 
-dWizardActivityPage[WV_SKIP_PERSONALIZED_EXPERIENCE]=u'''
+dWizardActivityPage[WV_SKIP_PERSONALIZED_EXPERIENCE] = u'''
     # Then bypass personalize experience registration currently, i should press the skip button
     # Then sleep 1 seconds
-    # Then Wait until Text startwith "Let us personalize" appears on screen, timeout "10" seconds
+    # Then Wait until Text startwith "Let us personalize" appears on screen, timeout "60" seconds
     #   And sleep 1 seconds
     #   And Fail if the resources-id"com.tinklabs.activateapp:id/tv_skip" not appears
     #   And tap on button "android.widget.TextView":"resource-id":"com.tinklabs.activateapp:id/tv_skip"
@@ -67,7 +68,7 @@ dWizardActivityPage[WV_SKIP_PERSONALIZED_EXPERIENCE]=u'''
 '''
 
 
-dWizardActivityPage[WV_PASS_PLAY_VIDEO]=u'''
+dWizardActivityPage[WV_PASS_PLAY_VIDEO] = u'''
     # # wait a moment for video to load
     # Then Wait until Video"com.tinklabs.launcher:id/video_view" appears, timeout 60 seconds
     #   And Fail if the Video"com.tinklabs.launcher:id/video_view" not appears
@@ -80,7 +81,7 @@ dWizardActivityPage[WV_PASS_PLAY_VIDEO]=u'''
 
 # Tutorial Image 1
 # Tutorial Image 2
-dWizardActivityPage[WV_PASS_TUTORIAL_IMAGE]=u'''
+dWizardActivityPage[WV_PASS_TUTORIAL_IMAGE] = u'''
     Then Swipe "com.tinklabs.handyclock:id/gl_container" LEFT Distance "200" until "Let's start" appears on screen (max swipe "5")
         And tap on text "Let's start"
 '''
