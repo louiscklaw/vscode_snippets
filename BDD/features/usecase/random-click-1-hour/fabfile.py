f1cb24cda2664656899611d3b4adff62d29938f2
louis.law <louis.law@tinklabs.com>
2017-10-16 16:42:59 +0800

    init commit for fabfile,

---
+++ BDD/features/usecase/random-click-1-hour/fabfile.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding:utf-8
+import os, sys
+import logging
+import traceback
+from pprint import pprint
+
+logging.basicConfig(level=logging.DEBUG,
+   format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+   datefmt='%a, %d %b %Y %H:%M:%S',
+   filename='debug.log',
+   filemode='a')
+
+from fabric.api import cd,run, local
+from datetime import datetime
+today = datetime.now().strftime('%d%m%Y-%H%M%S')
+
+
+def run_test(command):
+    # NOTE: behave random-click-1-hour_selftest.feature --tags=test_swipe_feed_trending -vk
+    log_file_filename = today + '.out'
+    local('%s |tee %s' % (command, log_file_filename))
+
+
+def run_test_M812_selftest():
+    # NOTE: test procedure for M812
+    logging.error('for T1 only')
+    # run_test('behave  random-click-1-hour_selftest.feature')
+    pass
+
+
+def run_test_VZH_selftest():
+    # NOTE: test procedure for VZH
+    run_test('behave  random-click-1-hour_selftest.feature')
+    pass
