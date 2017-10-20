aac2b1b02a952a371258b136146013d7be0c5ad1
louis.law <louis.law@tinklabs.com>
2017-10-20 09:36:49 +0800

    working copy of fabfile.py,

--- BDD/features/usecase/random-click-1-hour/fabfile.py
+++ BDD/features/usecase/random-click-1-hour/fabfile.py
@@ -14,6 +14,8 @@ logging.basicConfig(level=logging.DEBUG,
 
 from fabric.api import cd, run, local, env
 from datetime import datetime
+from time import sleep
+
 
 def get_today_string():
     return datetime.now().strftime('%d%m%Y-%H%M%S')
@@ -52,9 +54,28 @@ def destroy_appium_process(android_serial):
 
 
 def run_test(command):
+    """
+    behave test runner
+
+    Args:
+        command: command to run using behave (including behave).
+
+    """
     # NOTE: behave random-click-1-hour_selftest.feature --tags=test_swipe_feed_trending -vk
     log_file_filename = get_today_string() + '.out'
     local('%s |tee %s' % (command, log_file_filename))
+
+def run_command(command):
+    """
+    run the test without log file
+
+    Args:
+        command: command to be executed
+    """
+    logging.debug('starting running of command %s' % command)
+    local('%s' % command)
+
+    logging.debug('clear done')
 
 
 def run_test_M812_selftest():
@@ -68,6 +89,119 @@ def run_test_VZH_selftest():
     # NOTE: test procedure for VZH
     run_test('behave  random-click-1-hour_selftest.feature')
     pass
+
+def setup_daily_log(base_path, base_filename):
+    """
+    procedure to setup log file(daily log)
+
+    Args:
+        base_path: the path to collect the log file
+
+    """
+    daily_log_endstring = get_today_string() + '.log'
+    daily_log_filename = base_filename.replace(
+        ".", '_') + '_' + daily_log_endstring
+
+    logging.basicConfig(level=logging.DEBUG,
+                        filename=base_path + daily_log_filename,
+                        filemode='a')
+
+
+def construct_beahve_command(feature_file_name, feature_result_filename='', tags=''):
+    """
+    procedure to create behave command
+
+    Args:
+        feature_file_name: the feature file want to execute
+        tags: the tags wanted, default to 'no-tags'
+            '' - no tag
+            tag1 - single tag
+            tag1,tag2,tag3... = multitag example 1
+            tag1,-tag2,tag3... = multitag example 2
+
+
+    """
+    behave_command = ''
+    result_parameter = ''
+
+    if tags == '':
+        behave_command = 'behave %s' % feature_file_name
+        pass
+
+    else:
+        # NOTE: defined tags, construct tags parameters
+        # NOTE: normanize tags by ,
+
+        # TODO: inprogress
+        pass
+
+    if feature_result_filename == '':
+        pass
+    else:
+        logging.debug('feature_result_filename:%s' % feature_result_filename)
+        result_parameter = ' | tee %s' % feature_result_filename
+        behave_command += result_parameter
+
+
+    return behave_command
+
+
+def run_never_end_test(feature_file_name, result_collect_directory, beahve_tags=''):
+    """
+    keep running of the test
+
+    Args:
+        feature_file_name: the .feature file want to execute
+        result_collect_directory: result collection directory
+        behave_tags: the tags wanted
+
+    Doc:
+        http://docs.fabfile.org/en/1.13/usage/fab.html -> Per-task arguments
+
+    NOTE: Example
+        fab run_never_end_test:random-click-1-hour_quick_T1.feature,./result/T1
+        fab run_never_end_test:random-click-1-hour_quick_M812.feature,./result/M812
+
+    """
+
+    while 1:
+        sleep(1)
+        logging.debug('result_collect_directory:%s' % result_collect_directory)
+        log_file_filename = result_collect_directory + '/' + get_today_string() + '.out'
+
+
+        behave_execution_log_path = './behave_log/'
+        setup_daily_log(behave_execution_log_path, feature_file_name)
+
+        logging.error('feature_file_name:%s' % feature_file_name)
+
+        try:
+            print('start')
+            print('logfilename: %s' % log_file_filename)
+            logging.debug('start run the feature file: %s :' % feature_file_name)
+            # logging.debug(
+            #     run_command('behave %s | tee ./result/%s' % (feature_file_name, log_file_filename))
+            # )
+            # local('fastboot reboot')
+            logging.debug(
+                run_command(
+                    construct_beahve_command(
+                        feature_file_name, log_file_filename, '')
+                    )
+            )
+
+        except FabricException as e:
+            logging.debug('ignore exception from Fabric')
+
+        except KeyboardInterrupt as e:
+            logging.debug('ctrl-c end')
+            print('receive ctrl-c, end myself')
+            raise e
+
+        else:
+            pass
+
+
 
 def run_llaw_localtest(tags,number_of_run):
     """
