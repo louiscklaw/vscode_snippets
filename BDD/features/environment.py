0a93c72c8c0e41b90ebb49b1b7518f4c2d544a7f
louis.law <louis.law@tinklabs.com>
2017-11-03 11:52:55 +0800

    remove extra logging,

--- BDD/features/environment.py
+++ BDD/features/environment.py
@@ -18,26 +18,7 @@ from config import *
 
 import logging
 
-# (sApkName, sHKOAppId) = ('../_apk/com.tinklabs.activateapp_base.apk', 'com.tinklabs.activateapp.features.wizard.WizardActivity')
-
-# def before_all(context):
-#     context.config.setup_logging()
-
-
-# def after_scenario(context):
-#     context.appiumSession.quit()
-
-# https://pythonhosted.org/behave/tutorial.html
-# Debug-on-Error (in Case of Step Failures)
-# USE: behave -D BEHAVE_DEBUG_ON_ERROR         (to enable  debug-on-error)
-# USE: behave -D BEHAVE_DEBUG_ON_ERROR=yes     (to enable  debug-on-error)
-# USE: behave -D BEHAVE_DEBUG_ON_ERROR=no      (to disable debug-on-error)
-BEHAVE_DEBUG_ON_ERROR = os.getenv("BEHAVE_DEBUG_ON_ERROR", False)
-
-
-def setup_debug_on_error(userdata):
-    global BEHAVE_DEBUG_ON_ERROR
-    BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
+from behave.log_capture import capture
 
 
 def quit_appiumSession(context):
@@ -46,39 +27,21 @@ def quit_appiumSession(context):
 
 
 def before_all(context):
-    setup_debug_on_error(context.config.userdata)
-    context.logger = setup_logger('handy_behave_run_logger')
     context.adb_binary = PATH_ADB_BINARY
 
     # behave initialization
     context.appium_port = []
     context.appium_pid = []
 
-    # -- SET LOG LEVEL: behave --logging-level=ERROR ...
-    # on behave command-line or in "behave.ini".
-    context.config.setup_logging()
-
-
-
     logging.basicConfig(level=logging.DEBUG)
+
 
 def after_all(context):
     pass
 
 
 def after_step(context, step):
-    if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
-        # -- ENTER DEBUGGER: Zoom in on failure location.
-        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
-        import ipdb
-        ipdb.post_mortem(step.exc_traceback)
-
-    # louis.law add capability to capture screen if possible
-    # if hasattr(context,'appiumSession'):
-    #     context.execute_steps(u'''
-    #         Then ADB screen capture, save to "/Users/louis_law/_temp"
-    #     ''')
-
+    pass
 
 def before_step(context, scenario):
     pass
@@ -111,6 +74,7 @@ def before_scenario(context, scenario):
     pass
 
 
+@capture
 def after_scenario(context, scenario):
     if hasattr(context, 'appiumSession'):
         # uninstall_app(context, sHKOAppId)
