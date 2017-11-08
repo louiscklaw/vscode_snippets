401649c8d7347425910ed95fb5ec8488a27cd89c
louis.law <louis.law@tinklabs.com>
2017-11-08 09:09:44 +0800

    stop before "Wait for handy initialization",

--- BDD/features/steps/appium_steps.py
+++ BDD/features/steps/appium_steps.py
@@ -426,18 +426,18 @@ def step_impl(context):
         Given adb binary is available
         Given appium is running
 
-          Given Fastboot init
-          Given FASTBOOT Erase userdata
+        Given Fastboot init
+        Given FASTBOOT Erase userdata
             And ADB Wait for device, timeout 600 seconds
 
             # about 600 for T1
             # about 900 for M812
             And ADB check boot completed, timeout 1200 seconds
 
-        Then Wait for handy initialization
-            # TODO: resume
-            # And inject wifi configuration WIFI_CONFIG_TINKLABS_WTTQA
-            And ADB Initialize android
+        # Then Wait for handy initialization
+        #     # TODO: resume
+        #     # And inject wifi configuration WIFI_CONFIG_TINKLABS_WTTQA
+        #     And ADB Initialize android
         ''')
         pass
     except Exception as e:
